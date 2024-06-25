07ffd8fa616497a2276c09d129afa0cdd06665b7
ninhnguyen2623 <ninh1160@gmail.com>
2024-06-25 15:38:40 +0700

    up header

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,15 +1,28 @@
 import Box from '@mui/material/Box'
 import ModeSelect from '../ModeSelect'
+import AppsIcon from '@mui/icons-material/Apps'
+import { ReactComponent as TrelloLogo } from '~/assets/trello.svg'
+import SvgIcon from '@mui/material/SvgIcon'
+import Typography from '@mui/material/Typography'
 function AppBar() {
   return (
-    <Box sx={{
-      backgroundColor: 'primary.light',
+    <Box px={2} sx={{
       width: '100%',
       height: (theme) => theme.trello.appBarHeight,
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center',
+      justifyContent: 'space-between'
     }}>
-      <ModeSelect/>
+      <Box sx={{ display: 'flex', alignItems: 'center', grap: 2 }}>
+        <AppsIcon sx={{ color: 'primary.main' }} />
+        <Box sx={{ display: 'flex', alignItems: 'center', grap : 0.5 }}>
+          <SvgIcon component={TrelloLogo} inheritViewBox sx={{ color: 'primary.main' }} />
+          <Typography variant='span' sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'primary.main' }}>Trello</Typography>
+        </Box>
+      </Box>
+      <Box>
+        <ModeSelect />
+      </Box>
     </Box>
   )
 }
